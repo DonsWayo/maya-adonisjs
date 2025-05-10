51c47dc81b6b078662e7d99a898c8e694df61406
Juan Jose Carracedo <34981526+DonsWayo@users.noreply.github.com>
2025-05-10 10:49:11 +0200

    refactor: migrate to domain-driven architecture with modular routing

--- apps/main/adonisrc.ts
+++ apps/main/adonisrc.ts
@@ -69,7 +69,7 @@ export default defineConfig({
 
     //auth
     () => import('#auth/start/view'),
-    () => import('#auth/start/events'),
+    //() => import('#auth/start/events'),
     () => import('#auth/routes'),
 
     //users
